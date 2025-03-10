d806d8b30000c8906cefedfdeaedbf9503744f5e
Fran Cortés <100516685+francordel@users.noreply.github.com>
2025-03-10 11:29:56 +0100

    customice

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,26 +2,27 @@ export const socials = [
   {
     title: 'Instagram',
     icon: 'mdi:instagram',
-    url: 'https://www.instagram.com/jaydanurwin/',
+    url: 'https://www.instagram.com/misintaxis/',
   },
   {
     title: 'Twitter',
     icon: 'mdi:twitter',
-    url: 'https://twitter.com/littlesticksdev',
+    url: 'https://twitter.com/misintaxis',
   },
-  {
+  /*{
     title: 'YouTube',
     icon: 'mdi:youtube',
     url: 'https://youtube.com/c/LittleSticks',
   },
+  */
   {
     title: 'GitHub',
     icon: 'mdi:github',
-    url: 'https://github.com/littlesticksdev',
+    url: 'https://github.com/francordel',
   },
   {
-    title: 'Dribbble',
+    title: 'francordel.ia',
     icon: 'mdi:dribbble',
-    url: 'https://dribbble.com/littlesticksdev',  
+    url: 'https://linktr.ee/francordel.ia',  
   }
 ]
